14c28488eac20be33989b04348c88dd8b2dc3287
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-16 22:45:38 +0530

    Update bar_chart.java

--- Functions and Arrays/Arrays/bar_chart.java
+++ Functions and Arrays/Arrays/bar_chart.java
@@ -11,13 +11,13 @@ public class Main{
         for (int i = 0; i< a.length; i ++)    a[i] = scn.nextInt();  // array input
         
         int max = a[0] ;
-        for (int i = 1; i< a.length; i ++) if (a[i] > max) max = a[i]; // we get the max value in array
+        for (int i = 1; i< a.length; i ++)    if (a[i] > max) max = a[i]; // we get the max value in array
     
-        for (int i = max; i > 0; i--){
+        for (int i = max; i > 0; i--){  // wehave to make a block of max x n and print stars acc to value of a[i]
             for (int j = 0; j< a.length; j ++) {
 
-                if (a[j] >= i) System.out.print("*\t");
-                else System.out.print("\t");
+                if (a[j] >= i)  System.out.print("*\t");
+                else            System.out.print("\t");
 
             }   
             System.out.println();
