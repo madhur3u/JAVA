1544b67aa6c0f33e874a98a985a6485fc6872fa3
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-16 22:41:17 +0530

    Add files via upload

---
+++ Functions and Arrays/anybase_subtraction.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int b = scn.nextInt();
+    int n1 = scn.nextInt();
+    int n2 = scn.nextInt();
+
+    int d = getDifference(b, n1, n2);
+    System.out.println(d);
+  }
+
+  public static int getDifference(int b, int n1, int n2) {
+    
+    int diff = 0, borrow = 0, p = 1;
+
+    while (n2 > 0){
+
+      int d1 = n1 % 10;
+      int d2 = n2 % 10;
+      n1 = n1/10;
+      n2 = n2/10;
+
+      int d = (d2 - borrow) - d1;
+      if (d<0){
+        borrow = 1;
+        d = d + b;
+      }
+      else borrow = 0;
+
+      diff += d*p;
+      p = p * 10;
+    }
+    return diff;
+  }
+
+}
