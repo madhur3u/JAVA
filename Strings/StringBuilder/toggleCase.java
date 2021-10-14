7a130eb07fc2260e392cf52a0bf9cf12edca45a6
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-10-14 17:46:30 +0530

    Update toggleCase.java

--- Strings/StringBuilder/toggleCase.java
+++ Strings/StringBuilder/toggleCase.java
@@ -2,9 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        System.out.println(toggleCase(str));
+    }
 
-	public static String toggleCase(String str){
-		//write your code here
+    public static String toggleCase(String str){
         StringBuilder sb = new StringBuilder(str);
         
         for(int i = 0; i< sb.length(); i++){
@@ -12,20 +17,12 @@ public class Main {
             char s = sb.charAt(i);
             
             if (s >='A' && s<='Z'){
-                sb.setCharAt(i, (char)((int)s+32));
+                sb.setCharAt(i, (char)((int)s+32));  // changing to lowercase
             }
             else{
-                sb.setCharAt(i, (char)((int)s-32));
-            }
-            
-            
+                sb.setCharAt(i, (char)((int)s-32));  // changing to uppercase
+            }   
         }
-		return sb.toString();
-	}
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		String str = scn.next();
-		System.out.println(toggleCase(str));
-	}
-
+        return sb.toString();
+    }
 }
